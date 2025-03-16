70a636b37efc440f0fea9534ef601d3026d81878
Wesley Sanches <wesley.sanches@bling.com.br>
2025-03-16 00:46:52 -0300

    chore: constantes para metodos

--- config/config.php
+++ config/config.php
@@ -4,10 +4,10 @@ define('DEFAULT_METHOD', 'index');
 define('NAMESPACE_CONTROLLER', 'App\\Controllers\\');
 define('MAINTENANCE_CONTROLLER', 'Principal\\MaintenanceController');
 
-define('URL_API', '');
+define('HTTP_METHOD_GET', 'GET');
+define('HTTP_METHOD_POST', 'POST');
+define('HTTP_METHOD_PUT', 'PUT');
+define('HTTP_METHOD_DELETE', 'DELETE');
+
 define('URL_BASE', 'http://localhost/modelos/modelo-mvc-twig/');
-define('BANCO', '');
-define('SERVIDOR', '');
-define('USUARIO', '');
-define('SENHA', '');
 define('MAINTENANCE', 0);
