29baa62c90dbdd5d2f03f30a9358b1ec6c4246dd
Wesley Sanches <wesley.sanches@bling.com.br>
2025-03-16 02:11:55 -0300

    chore: ajuste de chamada do trait

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -4,11 +4,11 @@ namespace App\Core;
 
 use App\Supports\Traits\LoadViewTwig;
 use App\Supports\SupportsCripto\Cripto;
-use App\Supports\Traits\HttpResponseTrait;
+use App\Supports\Traits\HttpRequestResponseTrait;
 
 class Controller
 {
     use Cripto;
     use LoadViewTwig;
-    use HttpResponseTrait;
+    use HttpRequestResponseTrait;
 }
