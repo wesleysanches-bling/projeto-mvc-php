c4437ab4b9a620e73e7febdee1a8200c1c2c563b
Wesley Sanches <wesley.sanches@bling.com.br>
2025-03-16 00:46:04 -0300

    chore: ajuste de conexão com a base de dados

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -4,7 +4,7 @@ namespace App\Core;
 
 use App\Supports\SupportsApirequest\PullApi;
 use App\Supports\SupportsCripto\Cripto;
-
+use Exception;
 
 abstract class Model
 {
@@ -12,11 +12,29 @@ abstract class Model
     protected $db;
     public function __construct()
     {
-        $opcoes = array(
+        $host = $_ENV['DB_HOST'];
+        $dbname = $_ENV['DB_DATABASE'];
+        $username = $_ENV['DB_USERNAME'];
+        $password = $_ENV['DB_PASSWORD'];
+        $port = $_ENV['DB_PORT'] ?? '3306';
+
+        if (!$host || !$dbname || !$username || !$password) {
+            throw new Exception("As variáveis de ambiente do banco de dados não estão configuradas corretamente.");
+        }
+
+        $opcoes = [
             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-            \PDO::MYSQL_ATTR_INIT_COMMAND => "Set NAMES utf8"
-        );
-        $this->db = new \PDO("mysql:dbname=" . BANCO . ";host=" . SERVIDOR, USUARIO, SENHA, $opcoes);
+            \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        ];
+
+        $dsn = "mysql:host={$host};dbname={$dbname};port={$port}";
+
+        try {
+            $this->db = new \PDO($dsn, $username, $password, $opcoes);
+        } catch (\PDOException $e) {
+            throw new Exception("Erro ao conectar ao banco de dados: " . $e->getMessage());
+        }
+
     }
 
     protected function pullApi($data, string $url)
