a35419a4600557cc79938da979896ecfc3f433b9
Wesley Sanches <wesley.sanches@bling.com.br>
2025-03-19 02:43:08 -0300

    feat: exemplo de menu nav

---
+++ resources/js/src/components/MenuNav.vue
@@ -0,0 +1,22 @@
+<template>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">MVC</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+        <router-link class="nav-link" to="/contact">Contato</router-link>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
