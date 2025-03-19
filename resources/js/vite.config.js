f7f79a172fc063dc779abfc7d6f867129750a4e2
Wesley Sanches <wesley.sanches@bling.com.br>
2025-03-19 00:40:04 -0300

    feat: estrutura inicial da aplicação vue

---
+++ resources/js/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: '../../public/assets/js/vue-app',
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'bundle.js',
+        chunkFileNames: 'chunk-[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      }
+    },
+    base: './'
+  }
+})
