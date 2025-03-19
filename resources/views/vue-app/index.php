6d7d4a8ccf69781f44351911f06d1515bb230dd9
Wesley Sanches <wesley.sanches@bling.com.br>
2025-03-19 00:39:42 -0300

    feat: view que renderiza o vue app compilado

---
+++ resources/views/vue-app/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="<?= URL_BASE ?>/public/assets/js/vue-app/assets/index.css">
+	</head>
+
+	<body>
+		<div id="app"></div>
+		<script type="module" src='<?= URL_BASE ?>/public/assets/js/vue-app/bundle.js' defer></script>
+	</body>
+</html>
